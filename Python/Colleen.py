af5a50bfd8c3344355141d2be9c727f345cfc169
hlouar <hamzalouar@gmail.com>
2017-09-14 23:00:31 +0200

    Sully done

--- Python/Colleen.py
+++ Python/Colleen.py
@@ -1,14 +1,11 @@
 # com1
 def titi():
  return ;
-def main():
- # com2
- titi();
- n='\n';
- b='\\';
- g='"';
- p='%';
- s="# com1%sdef titi():%s return ;%sdef main():%s # com2%s titi();%s n='%sn';%s b='%s%s';%s g='%s';%s p='%s';%s s=%s%s%s;%s print s%s(n, n, n, n, n, n, b, n, b, b, n,g, n, p, n, g, s, g, n, p, n, n);%s%smain()";
- print s%(n, n, n, n, n, n, b, n, b, b, n,g, n, p, n, g, s, g, n, p, n, n);
-
-main()
+# com2
+titi();
+n='\n';
+b='\\';
+g='"';
+p='%';
+s="# com1%sdef titi():%s return ;%s# com2%stiti();%sn='%sn';%sb='%s%s';%sg='%s';%sp='%s';%ss=%s%s%s;%sprint s%s(n, n, n, n, n, b, n, b, b, n,g, n, p, n, g, s, g, n, p);";
+print s%(n, n, n, n, n, b, n, b, b, n,g, n, p, n, g, s, g, n, p);
---
+++ Python/Sully.py
@@ -0,0 +1,15 @@
+# com2
+import os
+i=5;
+if (__file__ == "Sully.py"):
+ i = i + 1
+if (i - 1 >= 0):
+ n='\n';
+ b='\\';
+ g='"';
+ p='%';
+ s="# com2%simport os%si=%s%sif (__file__ == %sSully.py%s):%s i = i + 1%sif (i - 1 >= 0):%s n='%sn';%s b='%s%s';%s g='%s';%s p='%s';%s s=%s%s%s;%s file = open(%sSully_%s + str(i - 1) + %s.py%s,%sw%s);%s file.write(s%s(n, n, i - 1, n, g, g, n, n, n, b, n, b, b, n,g, n, p, n, g, s, g, n, g, g, g, g, g, g, n, p, n, n, g, g, g, g, n))%s file.close()%s os.system(%spython Sully_%s + str(i - 1) + %s.py%s)%s";
+ file = open("Sully_" + str(i - 1) + ".py","w");
+ file.write(s%(n, n, i - 1, n, g, g, n, n, n, b, n, b, b, n,g, n, p, n, g, s, g, n, g, g, g, g, g, g, n, p, n, n, g, g, g, g, n))
+ file.close()
+ os.system("python Sully_" + str(i - 1) + ".py")
